a819cd48022fb27a89b1d7ed6a351d46e536d3b1
YouYou <47890166+MashiroW@users.noreply.github.com>
2021-10-02 18:55:39 +0200

    Add files via upload

--- space_invaders.c
+++ space_invaders.c
@@ -7,28 +7,42 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <sys/time.h>
- 
+
 //PLAYER
 #define PLAYER_HP 10
 #define PLAYER_SPEED 4
 #define PLAYER_X_MIN 5
-#define PLAYER_X_MAX 82.00
+#define PLAYER_X_MAX 80.00
+ 
+// X: 190 - Y - 52
  
 //ENEMIES
-#define MINION_NBR 5
+#define MINION_NBR 6
+#define MINION_MAX_PER_LINE 6
  
 //RESSOURCES
+#define LEVIATHAN "./models/leviathan/leviathan_default.txt"
 #define MINION_DEFAULT "./models/minion/minion_default.txt"
 #define MINION_LEFT "./models/minion/minion_left.txt"
 #define MINION_RIGHT "./models/minion/minion_right.txt"
-#define PLAYER "./models/player/player_default.txt"
-#define BACKGROUND "./models/background/stars02.txt"
- 
+#define PLAYER "./models/player/player_default2.txt"
+#define BACKGROUND01 "./models/background/stars01.txt"
+#define BACKGROUND02 "./models/background/stars02.txt"
+
+#define TIMECHECK \
+    if ( (last_timestamp + 0.7) < timestamp()) \
+    { \
+        last_timestamp = timestamp(); \
+        background_status = !background_status; \
+        display_update(element, minions, captains, leviathan, background_status); \
+    } \
+
 #define clear() printf("\033[H\033[J")
  
 typedef struct player player;
 struct player
 {
+    int HP;
     int posX;
     int posY;
 };
@@ -39,7 +53,27 @@ struct minion
     int HP;
     int posX;
     int posY;
-    int direction; // D:1 - E:2 - W:3
+    int model; // D:1 - E:2 - W:3
+    int speed;
+};
+
+typedef struct captain captain;
+struct captain
+{
+    int HP;
+    int posX;
+    int posY;
+    int model; // D:1 - E:2 - W:3
+    int speed;
+};
+
+typedef struct leviathan leviathan;
+struct leviathan
+{
+    int HP;
+    int posX;
+    int posY;
+    int speed;
 };
  
 typedef struct bullet bullet;
@@ -47,21 +81,11 @@ struct bullet
 {
     int posX;
     int posY;
+    int direction; // UP:1 - DOWN:0
+    int speed;
     float lastupdate;
 };
- 
-void get_time(double secs)
-{
-    struct timeval start, stop;
-    gettimeofday(&start, NULL);
- 
-    // Do stuff  here
- 
-    gettimeofday(&stop, NULL);
-    secs += (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
-    printf("time taken %f\n",secs);
-}
- 
+
 char key_pressed()
 {
     struct termios oldterm, newterm;
@@ -106,19 +130,21 @@ void display_element_XY(int x, int y, char *file_path)
     }
 }
  
-void display_background()
+void display_background(bool background_status)
 {
     printf("\033[%d;%dH", 1,1);
  
     int c;
     FILE *file;
-    file = fopen(BACKGROUND, "r");
+
+    if (background_status == false) file = fopen(BACKGROUND01, "r");
+    else file = fopen(BACKGROUND02, "r");
+
     if (file) 
     {
         while ((c = getc(file)) != EOF)
         {
-            if ( c != ' ')
-                printf("%c", c);                 
+            printf("%c", c);
         }
  
         fclose(file);
@@ -130,62 +156,109 @@ void minion_update(minion element)
     clear();
     //display_background();
     
-    if (element.direction == 1)
+    if (element.model == 1)
         display_element_XY(element.posX, element.posY, MINION_DEFAULT); 
-    else if (element.direction == 2)
+    else if (element.model == 2)
         display_element_XY(element.posX+5, element.posY-1, MINION_LEFT);
-    else if (element.direction == 3)
+    else if (element.model == 3)
         display_element_XY(element.posX+5, element.posY-1, MINION_RIGHT);
 }
  
-void player_update(player element)
+void display_minions(minion *minions, leviathan *leviathan)
+{
+    for(int a = 0; a < MINION_NBR; a++)
+    {
+        if ( minions[a].model == 1) display_element_XY(minions[a].posX, minions[a].posY, MINION_DEFAULT);
+        else if ( minions[a].model == 2) display_element_XY(minions[a].posX, minions[a].posY, MINION_LEFT);
+        else if ( minions[a].model == 3) display_element_XY(minions[a].posX, minions[a].posY, MINION_RIGHT);
+    }
+
+    display_element_XY(leviathan[0].posX, leviathan[0].posY, LEVIATHAN);
+}
+
+void display_update(player *element, minion *minions, captain *captains, leviathan *leviathan, bool background_status)
 {
     clear();
-    //display_background();
-    display_element_XY(element.posX, element.posY, PLAYER); 
+    display_background(background_status);
+    display_element_XY((*element).posX, (*element).posY, PLAYER);
+    display_minions(minions, leviathan);
 }
  
-void player_listen(player *element)
+double timestamp()
 {
-    char input; 
-    input = key_pressed();
- 
-    // Esc
-    if (input == 27) //While Escape not pressed
+    struct timeval ts;
+    gettimeofday(&ts, NULL); // return value can be ignored
+
+    double secs;
+    double usecs; 
+    double timestamp;
+    secs = ts.tv_sec;
+    usecs = ts.tv_usec;
+
+    while (usecs > 1) usecs = usecs/10;
+
+    timestamp = secs + usecs;
+    //printf("secs = %f -- usecs = %f -- timestamp = %f --\n", secs, usecs, timestamp);
+
+    return timestamp;
+}
+
+void player_listen(player *element, minion *minions, captain *captains, leviathan *leviathan)
+{
+    bool background_status = false;
+    double last_timestamp = timestamp();
+
+    display_update(element, minions, captains, leviathan, background_status);
+    
+
+    while(1)
     {
-        clear();
-        exit(0);
-    }
- 
-    // D INPUT
-    else if (input == 'd' && (*element).posX < PLAYER_X_MAX)
-    {
-        if ( (*element).posX + PLAYER_SPEED > PLAYER_X_MAX )
+        TIMECHECK
+
+        char input; 
+        input = key_pressed();
+    
+        // Esc
+        if (input == 27) //While Escape not pressed
         {
-            (*element).posX+=1;
-            player_update(*element);
+            clear();
+            exit(0);
         }
- 
-        else
+    
+        // D INPUT
+        else if (input == 'd' && (*element).posX < PLAYER_X_MAX)
         {
-            (*element).posX+=PLAYER_SPEED;
-            player_update(*element);
+            if ( (*element).posX + PLAYER_SPEED > PLAYER_X_MAX )
+            {
+                (*element).posX+=1;
+                TIMECHECK
+                display_update(element, minions, captains, leviathan, background_status);
+            }
+    
+            else
+            {
+                (*element).posX+=PLAYER_SPEED;
+                TIMECHECK
+                display_update(element, minions, captains, leviathan, background_status);
+            }
         }
-    }
- 
-    // Q INPUT
-    else if (input == 'q' && (*element).posX > PLAYER_X_MIN)
-    {
-        if ( (*element).posX - PLAYER_SPEED < PLAYER_X_MIN )
+    
+        // Q INPUT
+        else if (input == 'q' && (*element).posX > PLAYER_X_MIN)
         {
-            (*element).posX-=1;
-            player_update(*element);
-        }
- 
-        else
-        {
-            (*element).posX-=PLAYER_SPEED;
-            player_update(*element);
+            if ( (*element).posX - PLAYER_SPEED < PLAYER_X_MIN )
+            {
+                (*element).posX-=1;
+                TIMECHECK
+                display_update(element, minions, captains, leviathan, background_status);
+            }
+    
+            else
+            {
+                (*element).posX-=PLAYER_SPEED;
+                TIMECHECK
+                display_update(element, minions, captains, leviathan, background_status);
+            }
         }
     }   
 }
@@ -198,22 +271,63 @@ minion * minion_init()
     for(a = 0; a < MINION_NBR; a++)
     {
         minions[a].HP = 5;
-        minions[a].posX = PLAYER_X_MAX - (PLAYER_X_MAX/100)*70 + (((PLAYER_X_MAX/100)*70)/MINION_NBR)*a;
-        minions[a].posY = 4;
-        minions[a].direction = 1;
+        minions[a].posX = PLAYER_X_MAX - (PLAYER_X_MAX/100)*70 + (((PLAYER_X_MAX/100)*70)/MINION_NBR)*(a+1);
+        minions[a].posY = 10;
+        minions[a].model = 1;
+        minions[a].speed = 1;
     }
  
     //Display content of my array or minions
-    //for(a = 0; a < MINION_NBR; a++) printf("---- Minion #%d\nHP = %d\nPosX = %d\nPosY = %d\nDirection = %d\n------------\n\n", a+1, minions[a].HP, minions[a].posX, minions[a].posY, minions[a].direction);
+    //for(a = 0; a < MINION_NBR; a++) printf("---- Minion #%d\nHP = %d\nPosX = %d\nPosY = %d\nmodel = %d\n------------\n\n", a+1, minions[a].HP, minions[a].posX, minions[a].posY, minions[a].model);
  
     return minions;    
+}
+
+captain * captain_init()
+{
+    int a;
+    captain *captains = malloc(MINION_NBR*sizeof(captain)); 
+ 
+    for(a = 0; a < MINION_NBR/2; a++)
+    {
+        captains[a].HP = 5;
+        captains[a].posX = PLAYER_X_MAX/2;
+        captains[a].posY = 10;
+        captains[a].model = 1;
+        captains[a].speed = 2;
+    }
+  
+    return captains;    
+}
+
+leviathan * leviathan_init()
+{
+    leviathan *leviathan = malloc(sizeof(leviathan));
+    (*leviathan).HP = 100;
+    (*leviathan).posX = PLAYER_X_MAX/2;
+    (*leviathan).posY = 3;
+    (*leviathan).speed = 3;
+
+    return leviathan;
+}
+ 
+bullet * bullet_init(int posX)
+{
+    int a;
+    bullet *bullet = malloc(sizeof(bullet));
+    bullet[a].posX = PLAYER_X_MAX;
+    bullet[a].posY = 4;
+    bullet[a].speed = 4;
+ 
+    return bullet;    
 }
  
 player * player_init()
 {
     player *player = malloc(sizeof(player));
+    (*player).HP = 10;
     (*player).posX = 50;
-    (*player).posY = 35;
+    (*player).posY = 50;
  
     return player;
 }
@@ -221,18 +335,113 @@ player * player_init()
 int main()
 {   
     minion *minions = minion_init(); //Access element with "minions[0].posX)"
+    captain *captains = captain_init(); 
+    leviathan *leviathan = leviathan_init(); 
+
     player *player = player_init(); // Access element with "(*player).posX)"
- 
-    player_update((*player));
- 
-    while (1) //GAME
-    {
-        player_listen(player);
-    }
+
+    //GAME
+    player_listen(player, minions, captains, leviathan);
  
     // FREE, CLEAR SCREEN, QUIT    
     free(minions);
     free(player);
+    free(leviathan);
     clear();
     return 0;
 }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+/*
+int main()
+{
+    minion m1 = {
+        .HP = 10
+        .posX = 5,
+        .posY = 5,
+        .model = 1
+        .is_dead = false;
+    };
+ 
+    char input; 
+    double secs = 0;
+ 
+    clear();
+    update_display(m1);
+ 
+    while (1) //GAME
+    {
+        input = key_pressed();
+ 
+        // Esc
+        if (input == 27) //While Escape not pressed
+        {
+            clear();
+            exit(0);
+        }
+ 
+        // Z INPUT
+        else if (input == 'z' && m1.posY >= 1)
+        {
+            m1.posY-=1;
+            update_display(m1);
+        }
+ 
+        // S INPUT
+        else if (input == 's' && m1.posY >= 1)
+        {
+            m1.posY+=1;
+            update_display(m1);
+        }
+ 
+        // D INPUT
+        else if (input == 'd' && m1.posX <= LIMIT_X)
+        {
+            m1.posX+=PLAYER_SPEED;
+            update_display(m1);
+        }
+ 
+        // Q INPUT
+        else if (input == 'q' && m1.posX >= 0)
+        {
+            m1.posX-=PLAYER_SPEED;
+            update_display(m1);
+        }
+        
+        // J INPUT
+        else if (input == 'j') 
+        {
+            m1.model = 2;
+            update_display(m1);
+        }
+ 
+        // K INPUT
+        else if (input == 'k') 
+        {
+            m1.model = 1;
+            update_display(m1);
+        }
+    
+        // L INPUT
+        else if (input == 'l') 
+        {
+            m1.model = 3;
+            update_display(m1);
+        }
+ 
+    }
+        
+    clear();
+    return 0;
+}
+ 
+*/
